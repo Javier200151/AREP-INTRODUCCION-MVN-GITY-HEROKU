b84288fdce20c7028dd35ba30e8fa6b23e737a12
jelopez <jelope@asesoftware.com>
2021-08-20 09:02:41 -0500

    Spark implementación

--- src/main/java/co/edu/escuelaing/arep/sparkprimer/App.java
+++ src/main/java/co/edu/escuelaing/arep/sparkprimer/App.java
@@ -1,13 +1,14 @@
 package co.edu.escuelaing.arep.sparkprimer;
 
+import static spark.Spark.*;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("The app is starting ...!");
+        get("/hello", (req, res) -> "Hello World");
     }
 }
