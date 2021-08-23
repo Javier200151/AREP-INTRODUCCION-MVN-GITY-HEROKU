a7330048acbcce5c21cf30589f41e191155c3fa9
jelopez <jelope@asesoftware.com>
2021-08-23 13:34:42 -0500

    Pom update

--- src/main/java/co/edu/escuelaing/arep/sparkprimer/cache/IHistoricoCache.java
+++
@@ -1,5 +0,0 @@
-package co.edu.escuelaing.arep.sparkprimer.cache;
-
-public interface IHistoricoCache {
-    
-}
--- src/main/java/co/edu/escuelaing/arep/sparkprimer/cache/impl/HistoricoCache.java
+++
@@ -1,6 +0,0 @@
-package co.edu.escuelaing.arep.sparkprimer.cache.impl;
-import co.edu.escuelaing.arep.sparkprimer.cache.IHistoricoCache;
-
-public class HistoricoCache implements IHistoricoCache{
-    
-}
